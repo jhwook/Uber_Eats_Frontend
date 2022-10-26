78ca4e1fa65fe9f7e8cde5c408050e3447eacafc
jhwook <jhwooook@naver.com>
2022-10-27 00:30:20 +0900

    Login-form/tailwindcss-apply

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,22 +1,61 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface ILoginForm {
+  email?: string;
+  password?: string;
+}
 
 export const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<ILoginForm>();
+  const onSubmit = () => {};
   return (
     <div className="flex h-screen items-center justify-center bg-gray-800">
-      <div className="w-full max-w-lg rounded-lg bg-white py-10 text-center">
+      <div className="w-full max-w-lg rounded-lg bg-white pt-10 pb-7 text-center">
         <h3 className="text-2xl text-gray-800">Log In</h3>
-        <form className="mt-5 flex flex-col px-5">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mt-5 grid gap-3 px-5"
+        >
           <input
+            {...register("email", { required: "Email is required" })}
+            name="email"
+            required
+            type="email"
             placeholder="Email"
-            className=" mb-3 rounded-lg   border-2 bg-gray-100 py-3 px-5 shadow-inner focus:border-green-600 focus:border-opacity-60 focus:outline-none"
+            className="input"
           />
+          {errors.email?.message && (
+            <span className="font-medium text-red-500">
+              {errors.email?.message}
+            </span>
+          )}
           <input
+            {...register("password", {
+              required: "Password is required",
+              minLength: 10,
+            })}
+            required
+            name="password"
+            type="password"
             placeholder="Password"
-            className=" rounded-lg border-2 bg-gray-100 py-3 px-5 shadow-inner  focus:border-green-600 focus:border-opacity-60 focus:outline-none"
+            className="input"
           />
-          <button className="mt-3 rounded-lg bg-gray-800 py-3 px-5 text-lg text-white hover:opacity-90 focus:outline-none">
-            Log In
-          </button>
+          {errors.password?.message && (
+            <span className="font-medium text-red-500">
+              {errors.password?.message}
+            </span>
+          )}
+          {errors.password?.type === "minLength" && (
+            <span className="font-medium text-red-500">
+              Password must be more than 10 chars.
+            </span>
+          )}
+          <button className="btn mt-3">Log In</button>
         </form>
       </div>
     </div>
